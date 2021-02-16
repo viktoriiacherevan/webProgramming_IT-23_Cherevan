c291950c62f3c8f85197b03d05690044cd1adee2
viktoriiacherevan <72121691+viktoriiacherevan@users.noreply.github.com>
2021-02-16 12:19:41 +0200

    Update tablevent.js

--- js/tablevent.js
+++ js/tablevent.js
@@ -1,9 +1,8 @@
 document.getElementById('tab').addEventListener("mouseover", function () {
-    this.style.border = "5px solid tomato";
+    this.style.border = "7px solid tomato";
   })
    
   document.getElementById('tab').addEventListener("mouseout", function () {
     this.style.border = "none";
-    this.style.borderBottom = "2px solid tomato";
   })
   
